4ace347289e0fe6cbd77738ecd4c42a7cdbfbee4
origamidance <origamidance@gmail.com>
2020-01-08 00:09:25 +0100

    added ci_make_lib

--- cmake/CinderElla.cmake
+++ cmake/CinderElla.cmake
@@ -1,36 +1,11 @@
 set(CINDERELLA ON)
 
-option(USE_CCACHE "using ccache" ON)
-option(USE_ZAPCC "using zapcc" OFF)
-option(USE_COTIRE "using cotire" OFF)
-if (USE_CCACHE)
-# Configure CCache if available
-find_program(CCACHE_FOUND ccache)
-message("ccache:${CCACHE_FOUND}")
-if(CCACHE_FOUND)
-        set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
-        set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ccache)
-        set(ENV{CCACHE_SLOPPINESS} "pch_defines,time_macros")
-endif(CCACHE_FOUND)
-endif (USE_CCACHE)
-
-if (USE_COTIRE)
-    include(cotire)
-endif (USE_COTIRE)
-
-if (USE_ZAPCC)
-    SET(CMAKE_C_COMPILER /home/origamidance/dependencies/zapcc/build/bin/zapcc)
-    SET(CMAKE_CXX_COMPILER /home/origamidance/dependencies/zapcc/build/bin/zapcc++)
-endif (USE_ZAPCC)
-
-set(CMAKE_CXX_STANDARD 14)
-set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-
 find_package(Boost REQUIRED COMPONENTS system filesystem)
 
 get_filename_component( CINDER_PATH "${CMAKE_CURRENT_LIST_DIR}/../Cinder" ABSOLUTE )
 # get_filename_component( CINDER_PATH "/home/origamidance/Playground/Cinder" ABSOLUTE )
 get_filename_component( BLOCKS_PATH "${CMAKE_CURRENT_LIST_DIR}/../blocks" ABSOLUTE )
 include("${CINDER_PATH}/proj/cmake/modules/cinderMakeApp.cmake" )
-include("${CMAKE_CURRENT_LIST_DIR}/modules/cinderMakePythonBinding.cmake")
-include_directories("${CMAKE_CURRENT_LIST_DIR}/../external/Eigen")
+include("${CMAKE_CURRENT_LIST_DIR}/modules/cinderMakeLib.cmake")
+# include("${CMAKE_CURRENT_LIST_DIR}/modules/cinderMakePythonBinding.cmake")
+# include_directories("${CMAKE_CURRENT_LIST_DIR}/../external/Eigen")
